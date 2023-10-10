58ec6e86f0b3b11f47387cc085de1a5ee30c9773
johnhuitsai <caiqizhe@gmail.com>
2023-10-10 22:18:35 +0800

    Refactor auth.rs to use a utility function for signature calculation

--- src/auth.rs
+++ src/auth.rs
@@ -1,10 +1,8 @@
 use base64::Engine;
-use base64::engine::general_purpose::STANDARD;
-
 use crypto::digest::Digest;
-use crypto::sha1::Sha1;
 use log::debug;
 use serde_derive::{Deserialize, Serialize};
+use util::signature::get_signature;
 
 #[derive(Debug, Deserialize, Clone, Serialize)]
 pub struct AccessToken {
@@ -17,18 +15,6 @@ pub struct Signature {
     pub signature: String,
     pub timestamp: String,
     pub nonce: String,
-}
-
-
-pub fn get_signature(token: &str, timestamp: &str, nonce: &str, content: &str) -> String {
-    let content = STANDARD.encode(content.as_bytes()); // get base64 string of content
-    let mut v = [token, timestamp, nonce, content.as_str()];
-    v.sort();
-
-    let mut hasher = Sha1::new();
-    hasher.input_str(format!("{}{}{}{}", v[0], v[1], v[2], v[3]).as_str());
-
-    hasher.result_str()
 }
 
 pub fn check_signature(signature: &str, token: &str, timestamp: &str, nonce: &str, content: &str) -> bool {
